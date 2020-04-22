745cd5531ca734132df0a23c22ac5a5cbdc1ca69
Francis Samande Declaro <francis.declaro.092283@gmail.com>
2020-04-22 15:38:53 +0800

    Updates

---
+++ precache-manifest.d6bc44f2984bbe9fb0fe320364641a27.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af03af04d1b6b02f6cdde37e17a90a52",
+    "url": "/template/index.html"
+  },
+  {
+    "revision": "144b07be0d3e67099b10",
+    "url": "/template/static/js/main.dac30c6b.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/template/static/js/main.dac30c6b.js.LICENSE.txt"
+  }
+]);
